ebbfa90b45e7bdadc846190944ba68d86dcbb7a2
Theresah9979 <theresahb14@gmail.com>
2023-12-22 20:32:54 -0800

    modified Partners

--- components/Home/Partners.jsx
+++ components/Home/Partners.jsx
@@ -7,7 +7,7 @@ const Partners = () => {
     return (
         <section>
             <div className="flex flex-col justify-center items-center gap-8 bg-gray-100 px-24 py-16 overflow-hidden">
-                <div className="flex gap-8 items-center overflow-hidden w-[180%]">
+                <div className="flex gap-8 items-center overflow-hidden ">
                     {companies.map((item, index) => (
                         <div key={item.id} className="partners w-[10rem]">
                             <Image 
